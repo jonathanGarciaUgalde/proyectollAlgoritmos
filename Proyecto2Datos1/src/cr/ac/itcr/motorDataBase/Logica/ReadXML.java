e3e12b5869c2ff1a06d34156f075c409438ec943
Dilan Castro Enríquez <greekeshotmail.com>
2015-11-01 19:01:38 -0600

    Read XML

---
+++ Proyecto2Datos1/src/cr/ac/itcr/motorDataBase/Logica/ReadXML.java
@@ -0,0 +1,15 @@
+package cr.ac.itcr.motorDataBase.Logica;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+
+public class ReadXML {
+	
+
+}
