013b93fb74a293162dc5acaf50354fc1a4431e72
Gustavo Hernandez <ghernandezgranera16@gmail.com>
2015-11-06 15:19:43 -0600

    Tree Test

--- Proyecto2Datos1/src/cr/ac/itcr/motorDataBase/baseDatos/TreeTest.java
+++ Proyecto2Datos1/src/cr/ac/itcr/motorDataBase/baseDatos/TreeTest.java
@@ -5,6 +5,13 @@ public class TreeTest {
 
 	public static void main(String[] args) {
 		
+		ArbolAVL<Integer> arbol = new ArbolAVL<Integer>();
+		arbol.insertar(5);
+		arbol.insertar(7);
+		arbol.insertar(1);
+		arbol.insertar(3);
+		arbol.insertar(4);
+		arbol.preOrden(arbol.raiz);
 
 	}
 
