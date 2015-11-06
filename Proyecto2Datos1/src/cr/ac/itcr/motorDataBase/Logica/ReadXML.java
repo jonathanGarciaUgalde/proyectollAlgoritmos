60620480c7d438134f702ddc5c46f2e4e214387d
dcastro96 <dcastro96@LAIMI2-A09.estudiantes.tec.cr>
2015-11-06 16:07:56 -0600

    Cambios en leer XML

--- Proyecto2Datos1/src/cr/ac/itcr/motorDataBase/Logica/ReadXML.java
+++ Proyecto2Datos1/src/cr/ac/itcr/motorDataBase/Logica/ReadXML.java
@@ -2,6 +2,7 @@ package cr.ac.itcr.motorDataBase.Logica;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 import org.jdom2.Document;
 import org.jdom2.Element;
@@ -10,6 +11,30 @@ import org.jdom2.input.SAXBuilder;
 
 
 public class ReadXML {
+	
+	
+	public void borrarDato(Object dato,String nombre_indice){
+		File archivo=new File("archivo.xml");
+		SAXBuilder build=new SAXBuilder();
+		try{
+			Document doc=build.build(archivo);
+			Element raiz=doc.getRootElement();
+			
+			List indice=raiz.getChildren(nombre_indice);
+			Iterator iter=indice.iterator();
+			
+			while(iter.hasNext()){
+				Element elemento=(Element)iter.next();
+				if (elemento.equals(dato)){
+					iter.remove();
+				}
+				
+			}
+		}
+		catch(Exception e){
+			System.out.println("No se pudo borrar el elemento");
+		}
+	}
 	
 
 }
